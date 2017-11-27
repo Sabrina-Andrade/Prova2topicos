ea9aa0581837c2d96cc052e8d052451355878355
Sabrina-Andradegit config --global user.email saandradepereira@gmail.com <bcerqueira94@gmail.com>
2017-11-27 20:54:45 -0200

    Quinto commmit

--- prova-rest-ejb/src/main/java/br/edu/univas/si/ejb/beans/PedidoBean.java
+++ prova-rest-ejb/src/main/java/br/edu/univas/si/ejb/beans/PedidoBean.java
@@ -1,5 +1,31 @@
 package br.edu.univas.si.ejb.beans;
 
+import java.util.stream.Collectors;
+
+import javax.ejb.EJB;
+
+import br.edu.univas.si.ejb.dao.Pedidodao;
+import br.edu.univas.si.ejb.entities.Pedido;
+import br.edu.univas.si.ejb.shopping.interfaces.PedidoLocal;
+import br.edu.univas.si.ejb.shopping.interfaces.PedidoRemote;
+
 public class PedidoBean implements PedidoLocal, PedidoRemote {
+	
+	@EJB
+	private Pedidodao dao;
+
+	@Override
+	public void createNewPedido(String description, String order_time) {
+		Pedido pedido = new Pedido();
+		pedido.setDescription(description);
+		pedido.setOrder_time(order_time);
+		dao.insert(pedido);
+		
+	}
+
+	@Override
+	public String[] listPedidoCompra() {
+		return dao.listAll().stream().map(Pedido::getDescription).collect(Collectors.toList()).toArray(new String[0]);
+	}
 
 }
--- prova-rest-ejb/src/main/java/br/edu/univas/si/ejb/dao/Pedidodao.java
+++ prova-rest-ejb/src/main/java/br/edu/univas/si/ejb/dao/Pedidodao.java
@@ -1,8 +1,29 @@
 package br.edu.univas.si.ejb.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.edu.univas.si.ejb.entities.Pedido;
 
 @Stateless
 public class Pedidodao {
+	
+	@PersistenceContext(unitName = "pedido")
+	private EntityManager em;
+
+	public void insert(Pedido pedido) {
+		em.persist(pedido);
+	}
+
+	public List<Pedido> listAll() {
+		return em.createQuery("from pedido p", Pedido.class).getResultList();
+	}
+
+	
 
 }
+
+
